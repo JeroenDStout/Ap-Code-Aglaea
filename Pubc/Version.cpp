b946dadc81c00e4cd9bf55ea699baca15c43049c
Jeroen D Stout <jeroenstout@gmail.com>
2019-05-08 23:30:20 +0200

    Initial setup

---
+++ Pubc/Version.cpp
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "Aglaea/Pubc/Version.h"
+
+#include "Aglaea/-genc/def_repo_version.h"
+#include "Aglaea/-genc/def_contribute.h"
+
+namespace Aglaea {
+	namespace Core {
+		
+		BR_VERSION_DEFINE(Aglaea);
+        BR_CONTRIBUTE_DEFINE(Aglaea);
+
+	}
+} 
